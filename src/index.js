08250704f2e1c2abdd424fcd78e7710d37545246
Chris Pfaff <chrispfaff10@gmail.com>
2018-10-28 17:06:45 -0500

    added style loaders/babel dependencies and React to BoilerPlate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-(() => {
-	console.log('hello');
-})();
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+	return (
+		<div className="mainContainer">
+			<h1>Hello from React</h1>
+		</div>
+	);
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
